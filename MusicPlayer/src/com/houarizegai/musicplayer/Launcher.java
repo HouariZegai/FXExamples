d9a60271ffca58d889557def8b0bef2de7a5a1e9
houarizegai <houarizegai14@gmail.com>
2018-10-16 07:57:41 +0200

    Make Scene Transparent - 'Music Player'

--- MusicPlayer/src/com/houarizegai/musicplayer/Launcher.java
+++ MusicPlayer/src/com/houarizegai/musicplayer/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -14,7 +15,9 @@ public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/com/houarizegai/musicplayer/MusicPlayer.fxml"));
-        stage.setScene(new Scene(root));
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
         stage.initStyle(StageStyle.TRANSPARENT);
         Launcher.stage = stage;
         stage.show();
