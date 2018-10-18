4360b18ac4bc195879d0bf8cc6061cecea2cbbe1
houarizegai <houarizegai14@gmail.com>
2018-10-18 15:54:03 +0200

    Edit Dashboard UI & Adding Menu

--- Dashboard/src/com/houarizegai/dashboard/DashboardController.java
+++ Dashboard/src/com/houarizegai/dashboard/DashboardController.java
@@ -2,8 +2,13 @@ package com.houarizegai.dashboard;
 
 import com.jfoenix.controls.JFXDrawer;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,11 +18,46 @@ public class DashboardController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        initMenu();
+    }
 
+    private void initMenu() {
+        VBox menuPane = null;
+        try {
+            menuPane = FXMLLoader.load(getClass().getResource("/com/houarizegai/dashboard/menu/Menu.fxml"));
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        drawerMenu.setSidePane(menuPane);
+
+        for (Node node : menuPane.getChildren()) {
+            if (node.getAccessibleText() != null) {
+                node.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
+                    switch (node.getAccessibleText()) {
+                        case "Profile" :
+                            System.out.println("Profile Clicked !");
+                            break;
+                        case "Settings" :
+                            System.out.println("Settings Clicked !");
+                            break;
+                        case "Report" :
+                            System.out.println("Report Clicked !");
+                            break;
+                        case "Logout" :
+                            System.out.println("Logout Clicked !");
+                            break;
+                    }
+                });
+            }
+        }
     }
 
     @FXML
     private void onMenu() {
-
+        if (drawerMenu.isShown())
+            drawerMenu.close();
+        else
+            drawerMenu.open();
     }
 }
