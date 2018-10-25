f2be54f9a47947cdecc007f76637a20d97a7e7fd
houarizegai <houarizegai14@gmail.com>
2018-10-25 21:50:29 +0200

    Adding multi language app (internationalization)

---
+++ InternationalizationFx/src/sample/Controller.java
@@ -0,0 +1,56 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Label fieldUsername, fieldPassword;
+
+    ResourceBundle bundle;
+    Locale locale;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    private void setLangAR() {
+        loadLang("ar");
+    }
+
+    @FXML
+    private void setLangEN() {
+        loadLang("en");
+    }
+
+    @FXML
+    private void setLangFR() {
+        loadLang("fr");
+    }
+
+    private void loadLang(String lang) {
+        locale = new Locale(lang);
+        bundle = ResourceBundle.getBundle("sample.lang", locale);
+
+        fieldUsername.setText(toUTF(bundle.getString("username")));
+        fieldPassword.setText(toUTF(bundle.getString("password")));
+    }
+
+    private String toUTF(String val) {
+        try {
+            return new String(val.getBytes("ISO-8859-1"), "UTF-8");
+        } catch(Exception ex) {
+
+        }
+        return null;
+    }
+
+}
