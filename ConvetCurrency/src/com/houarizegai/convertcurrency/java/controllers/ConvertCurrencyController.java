2ba4bf3ca78234f641d557d2cf9439d284e6677a
HouariZEGAI <houarizegai14@gmail.com>
2018-11-21 17:23:50 +0100

    Adding From,To Currency (finishing Convert Engine) - ConvertCurrency

--- ConvetCurrency/src/com/houarizegai/convertcurrency/java/controllers/ConvertCurrencyController.java
+++ ConvetCurrency/src/com/houarizegai/convertcurrency/java/controllers/ConvertCurrencyController.java
@@ -12,10 +12,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class ConvertCurrencyController implements Initializable {
 
@@ -26,18 +33,34 @@ public class ConvertCurrencyController implements Initializable {
     private JFXTextField fieldMoney;
 
     @FXML
-    private JFXComboBox<String> comboCurreny;
+    private JFXComboBox<String> comboFromCurreny, comboToCurreny;
 
     @FXML
     private Label lblResult;
 
+    // Like Toast, an error Msg showing in the bottom of view
     private JFXSnackbar toastMsg;
+
+    // JSON contain value of currency
+    private JSONObject jsonCurrencyRates;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        comboCurreny.getItems().addAll("EURO", "DZD");
         toastMsg = new JFXSnackbar(root);
+
+        getCurrencyRatesFromRest();
+        initComboCurrency();
+    }
+
+    private void initComboCurrency() {
+        comboFromCurreny.getItems().addAll("Euro");
+        comboToCurreny.getItems().addAll("Euro");
+
+        jsonCurrencyRates.keys().forEachRemaining(item -> {
+            comboFromCurreny.getItems().add(item);
+            comboToCurreny.getItems().add(item);
+        });
     }
 
     @FXML
@@ -47,34 +70,82 @@ public class ConvertCurrencyController implements Initializable {
             return;
         }
 
-        if(comboCurreny.getSelectionModel().getSelectedItem() == null) {
-            toastMsg.show("Please Select Currency !", 2500);
+        if(comboFromCurreny.getSelectionModel().getSelectedItem() == null) {
+            toastMsg.show("Please Select From Currency !", 2500);
+            return;
+        }
+        if(comboToCurreny.getSelectionModel().getSelectedItem() == null) {
+            toastMsg.show("Please Select To Currency !", 2500);
             return;
         }
 
-        if(comboCurreny.getSelectionModel().getSelectedItem().equalsIgnoreCase("Euro")) {
-            lblResult.setText(Integer.parseInt(fieldMoney.getText().trim()) * getEuroValuesFromRest() + " DA");
+        double moneyAfterConvert = convertCurrency(comboFromCurreny.getSelectionModel().getSelectedItem(),
+                comboToCurreny.getSelectionModel().getSelectedItem(),
+                Integer.parseInt(fieldMoney.getText()));
+
+        lblResult.setText(String.valueOf(moneyAfterConvert));
+
+    }
+
+    private void getCurrencyRatesFromRest() { // Get JSON of currency rates from Rest API
+        try {
+//            HttpResponse<JsonNode> jsonResponse
+//                    = Unirest.get("http://data.fixer.io/api/latest")
+//                    .queryString("access_key", "8245004aec4e53232b7286456e039daf")
+//                    .asJson();
+//
+//            JSONObject jsonRoot = new JSONObject(jsonResponse.getBody().toString());
+
+            // Get Path of Project
+            Path currentRelativePath = Paths.get("");
+            // convert the path to absolute
+            String currentAbsolutePath = currentRelativePath.toAbsolutePath().toString();
+
+            String jsonCurrencyRatesPath = currentAbsolutePath + "\\src\\com\\houarizegai\\convertcurrency\\resources\\utils\\currencyRates.json";
+            this.jsonCurrencyRates = new JSONObject(readJsonFromFile(jsonCurrencyRatesPath))
+                    .getJSONObject("rates");
+
+        } catch(Exception ure) {
+            ure.printStackTrace();
         }
 
     }
 
-    private double getEuroValuesFromRest() {
+    private String readJsonFromFile(String pathname) { // Read json file and convert it to String
         try {
-            HttpResponse<JsonNode> jsonResponse
-                    = Unirest.get("http://data.fixer.io/api/latest")
-                    .queryString("access_key", "8245004aec4e53232b7286456e039daf")
-                    .asJson();
+            File file = new File(pathname);
 
-            JSONObject jsonRoot = new JSONObject(jsonResponse.getBody().toString());
+            StringBuilder fileContents = new StringBuilder((int) file.length());
 
-            JSONObject currencyObject = jsonRoot.getJSONObject("rates");
-            double dzdValue = currencyObject.getDouble("DZD");
+            try (Scanner scanner = new Scanner(file)) {
+                while (scanner.hasNextLine()) {
+                    fileContents.append(scanner.nextLine() + System.lineSeparator());
+                }
+                return fileContents.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-            return dzdValue;
-        } catch(UnirestException ure) {
-            ure.printStackTrace();
+    private double convertCurrency(String fromCurrency, String toCurrency, double money) {
+        double moneyResult = money;
+
+        double moneyInEuro;
+
+        if(fromCurrency.equalsIgnoreCase("EURO")) {
+            moneyInEuro = money;
+        } else {
+             moneyInEuro = money / jsonCurrencyRates.getDouble(fromCurrency);
         }
 
-        return 0;
+        if(toCurrency.equalsIgnoreCase("EURO")) {
+            moneyResult = moneyInEuro;
+        } else {
+            moneyResult = moneyInEuro * jsonCurrencyRates.getDouble(toCurrency);
+        }
+
+        return moneyResult;
     }
 }
